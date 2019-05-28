c948fb0afa6fa40dac8cb3f8aa8d35a493bf9645
Roberto Duralde <roduralde93@gmail.com>
2019-05-28 00:04:41 -0400

    Cambios

--- frontend/javascripts/Api_Communication_v2.py
+++ frontend/javascripts/Api_Communication_v2.py
@@ -1,13 +1,13 @@
 import requests
-from collections import OrderedDict
-from operator import itemgetter
 import json
 from flask import Flask
 from flask_cors import CORS
-from flask import request
 
+#Creación de API en servidor http://127.0.0.1:5000
 app = Flask(__name__)
+#Header de seguridad para que el navegador no bloquee el acceso
 CORS(app)
+#Ruta del servidor
 @app.route("/", methods=['GET', 'POST'])
 
 def getSortedDict():
@@ -15,9 +15,12 @@ def getSortedDict():
     URL_DEX = "https://pokeapi.co/api/v2/pokedex/1"
     dex = requests.get(url = URL_DEX)
     dex_data = dex.json()
+
+    #De los datos del pokedex obtenemos la cantidad total de pokemones
     total_id = len(dex_data["pokemon_entries"])  #pokemones totales (721)
     n_vista = 18
-    
+
+    #Obtención de datos requeridos: ID y Nombre
     poke_dict = {}
     poke_names = []
     poke_ids = []
@@ -29,7 +32,13 @@ def getSortedDict():
         poke_ids.append(poke_entry)
         poke_names.append(poke_name)
         poke_dict[poke_name] = poke_entry
-    
+
+    ### Se trabajará en listas por simplicidad, ya que los diccionarios no ###
+    ###             pueden separar sus elementos de manera fácil           ###
+    ### Posteriormente, los datos se pasan a un diccionario y formato json ###
+    ###              para su procesamiento en "connection_v2.js"           ###
+
+    #Ordenamos los nombres
     sorted_poke_names = sorted(poke_names)  
 
     #Primeros n_vista elementos que entran a la página
@@ -38,7 +47,7 @@ def getSortedDict():
     for i in range(n_vista):
         first_dict[first_names[i]] = poke_dict[first_names[i]]
 
-    #Preparamos la cola de total_id-n_vista elementos
+    #Preparamos la cola de (total_id-n_vista) elementos
     second_names = [item for item in sorted_poke_names if item not in first_names]
     second_dict = {}
     for name in second_names:
--- frontend/javascripts/connection_v2.js
+++ frontend/javascripts/connection_v2.js
@@ -1,9 +1,16 @@
-var address = 'http://127.0.0.1:5000/';
-var count = 0;
-var n_vista = 18;
-var ctrl = 1;
-var indice = 0;
+//SECCIÓN DE VARIABLES
+var address = 'http://127.0.0.1:5000/'; //Puerto del servidor
+var count = 0;  //Variable que recorre los datos
+var n_vista = 18; //Límite de vista requerido
+var ctrl = 1; //Para detectar el final de todos los pokemones
+var indice = 0; //Índice de posición en la tabla de vista de pokemones
 
+/*
+Cargamos el primero de los dos diccionarios enviados por el script de Python.
+El denominado "first_dict" (en el script de Python), son los primeros 18
+pokemones ordenados alfabéticamente, dejando 703 (total_id-n_vista) en el
+segundo diccionario.
+*/
 $(document).ready(function(){
   $.getJSON(address, function(data) {
     var init_data_keys = Object.keys(data[0]);
@@ -23,22 +30,33 @@ $(document).ready(function(){
 });
 
 function add_pokemons_to_list() {
+  /*Para evitar el llenado de la vista con "undefined", llevamos el control
+  de la cantidad de pokemones mostrados. Al llegar al total, esta variable
+  toma el valor de 0.
+  */
   if(ctrl){
       $.getJSON(address, function(data) {
-      var data_keys = Object.keys(data[1]);
-      var data_values = Object.values(data[1]);
-      var table = document.getElementById("table_pokemons");
-      if (indice == n_vista) {
-        indice = 0;
+        //Cargamos el segundo diccionario con los otros pokemones ordenados
+        var data_keys = Object.keys(data[1]);
+        var data_values = Object.values(data[1]);
+        var table = document.getElementById("table_pokemons");
+        /*Se reinicia la posición del índice para reemplazar el primer pokemon en la tabla
+        */
+        if (indice == n_vista) {
+          indice = 0;
         }
-      if (data_keys[count] == undefined || data_values[count] == undefined) {
-        alert("There's no more pokemons in the queue!")
-        ctrl = 0;
+        //Evaluación de fin de lista
+        if (data_keys[count] == undefined || data_values[count] == undefined) {
+          alert("There's no more pokemons in the queue!")
+          ctrl = 0;
         }
-      var id_col = table.rows[indice].cells[0]
-      var name_col = table.rows[indice].cells[1]
-      id_col.innerHTML = data_values[count];
-      name_col.innerHTML = data_keys[count];
+        //Posición de escritura dentro de la tabla
+        var id_col = table.rows[indice].cells[0]
+        var name_col = table.rows[indice].cells[1]
+
+        //Valor a escribir en la posición determinada
+        id_col.innerHTML = data_values[count];
+        name_col.innerHTML = data_keys[count];
       }
     )
   count += 1;
